f21c238165bb1cbef19755101a305fb85d07f123
SheTroxel <smtroxel@gmail.com>
2023-04-15 14:46:50 -0400

    updated app.py file to test

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import os
+import numpy as dt
 from flask import Flask, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.automap import automap_base
 
-Base = automap_base()
+    #code for pyscopg2 (if needed)
+    # Connect to your postgres DB
+    #conn = psycopg2.connect("dbname=test user=postgres")
+    # Open a cursor to perform database operations
+    #cur = conn.cursor()
+    # Execute a query
+    #cur.execute("SELECT * FROM my_data")
+    # Retrieve query results
+    #records = cur.fetchall()
 
 #engine = create_engine(os.gentenv(DATABASE_URI_E'))
 engine = create_engine(os.getenv('DATABASE_URI'))
@@ -13,7 +22,19 @@ Base.prepare(engine, reflect= True)
 
 Test = Base.classes.text
 
+session = Session(engine)
 
+app + Flask (__name__)
 
+@app.route('/')
+def greeting():
+    return 'Welcome to California Wildfire Data API'
+
+@app.route('/api')
+def test():
+    cols = ['incident_name','county', 'location', 'longitude', 'latitude','acres_burned', 'start_date', 'extinquished_date', 'days_duration']
+    data = session.query(Test).all()
+    result = [{col: getattr(d, col) for col in cols} for d in data]
+    return jsonify(result=result)
 
                        
