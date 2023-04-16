013b7694076a3235c44a18388c6343bf7e95e25f
SheTroxel <smtroxel@gmail.com>
2023-04-16 18:56:37 -0400

    fixed errors

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ from sqlalchemy.ext.automap import automap_base
 #VALUE: c124d7c8d7e7f920b99ff2a334b634b2
 #API Unique Name:   projectKey
 #API Key: rnd_7h1CIcrEbpcxg6tmAaZKcC58YZ4L
-#postgres://california_fire_data_user:NbkbeOnFEw8JiagLONTh7bO66LOj3OKQ@dpg-cgs9g8qut4mcloj9clbg-a.oregon-postgres.render.com/california_fire_data
 ############################################################################
+Base = automap_base
 
 #engine = create_engine(os.gentenv(DATABASE_URI_E'))
 engine = create_engine(os.getenv('DATABASE_URI'))
@@ -33,7 +33,7 @@ def greeting():
 def test():
     cols = ['incident_name','county', 'location', 'longitude', 'latitude','acres_burned', 'start_date', 'extinquished_date', 'days_duration']
     data = session.query(california_fire_data).all()
-    result = [{col: getattr(d, col) for col in cols} for d in data]
+    result = [{col: getattr(d, col, _default=None) for col in cols} for d in data]
     return jsonify(result=result)
 
-     
+    ##result = [{col: getattr(d, col) for col in cols} for d in data]
