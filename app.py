a277c8a1a22024e99199b453062565cf582b9350
SheTroxel <smtroxel@gmail.com>
2023-04-16 17:46:57 -0400

    updated links in app.py

--- app.py
+++ app.py
@@ -15,20 +15,6 @@ from sqlalchemy.ext.automap import automap_base
 #postgres://california_fire_data_user:NbkbeOnFEw8JiagLONTh7bO66LOj3OKQ@dpg-cgs9g8qut4mcloj9clbg-a.oregon-postgres.render.com/california_fire_data
 ############################################################################
 
-#connect to database
-#conn = psycopg2.connect(database="california_fire_data",
-						#user="california_fire_data_user",
-						#password="NbkbeOnFEw8JiagLONTh7bO66LOj3OKQ",
-						#host="dpg-cgs9g8qut4mcloj9clbg-a", port="5432")
-
-#cur = conn.cursor()
-#
-#conn.commit()
-#
-#cur.close()
-#conn.close()
-  
-
 #engine = create_engine(os.gentenv(DATABASE_URI_E'))
 engine = create_engine(os.getenv('DATABASE_URI'))
 
@@ -40,11 +26,11 @@ session = Session(engine)
 
 app + Flask (__name__)
 
-@app.route('/')
+@app.route('https://california-fire-data.onrender.com/')
 def greeting():
     return 'Welcome to California Wildfire Data API'
 
-@app.route('/api')
+@app.route('https://california-fire-data.onrender.com/api')
 def test():
     cols = ['incident_name','county', 'location', 'longitude', 'latitude','acres_burned', 'start_date', 'extinquished_date', 'days_duration']
     data = session.query(california_fire_data).all()
