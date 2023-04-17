c45d661aa9fe7773d94be32d7bb79e8e8319df33
SheTroxel <smtroxel@gmail.com>
2023-04-16 21:55:17 -0400

    more edits

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.automap import automap_base
-DATABASE_URI = "postgres://california_fire_data_user:NbkbeOnFEw8JiagLONTh7bO66LOj3OKQ@dpg-cgs9g8qut4mcloj9clbg-a.oregon-postgres.render.com/california_fire_data"
+#DATABASE_URI = "postgres://california_fire_data_user:NbkbeOnFEw8JiagLONTh7bO66LOj3OKQ@dpg-cgs9g8qut4mcloj9clbg-a.oregon-postgres.render.com/california_fire_data"
 
 #############################################################################
 #KEY: DATABASE_URI
@@ -17,7 +17,7 @@ Base = automap_base()
 
 #engine = create_engine(os.gentenv('DATABASE_URI_E'))
 #engine = create_engine(os.getenv(DATABASE_URI))
-engine = create_engine(DATABASE_URI)
+engine = create_engine("DATABASE_URI")
 
 Base.prepare(engine, reflect= True)
 
