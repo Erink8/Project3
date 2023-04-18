bc702f1f0d952e540eac46528a0f495e630228ec
nancygmz <gnancy775@gmail.com>
2023-04-17 22:50:10 -0400

    working app

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_cors import cross_origin
 
 
 # Create engine to postgress sql california wildfire data file
-engine = create_engine("postgres://climate_change_user:xEZhRWSSGx2TOWeWRDuS65K0D0UXcARD@dpg-cgutndodh87vdorr2910-a.oregon-postgres.render.com/california_climate_change")
+engine = create_engine("postgresql://climate_change_user:xEZhRWSSGx2TOWeWRDuS65K0D0UXcARD@dpg-cgutndodh87vdorr2910-a.oregon-postgres.render.com/california_climate_change")
 #engine = create_engine(os.getenv(DATABASE_URI))
 
 # Reflect an existing database into a new model
@@ -19,7 +19,7 @@ Base.prepare(engine,reflect=True)
 
 # Save references to each table
 Ca_wildfire = Base.classes.ca_wildfire
-# Ca_drought = Base.classes.ca_drought
+#Ca_drought = Base.classes.ca_drought
 
 # Initialize app
 app = Flask(__name__)
@@ -44,9 +44,13 @@ def test():
     results = session.query(Ca_wildfire).all()
     session.close()
 
-    cols = ['ID','incident_name', 'county', 'location', 'longitude', 'latitude', 'acres_burned', 'start_date', 'extinguished_date', 'days_duration','month_year']
+    cols = ['ID','incident_name', 'county', 'location', 'longitude', 'latitude', 'acres_burned', 'start_date', 'days_duration','month_year']
     data = session.query(Ca_wildfire).all()
-    result = [{col: getattr(d, col, _default=None) for col in cols} for d in data]
+   # print(getattr(data[0], 'ID'))
+
+
+
+    result = [{col: getattr(d, col) for col in cols} for d in data]
     return jsonify(result=result)
 
 if __name__ == "__main__":
