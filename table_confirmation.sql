5aacf44dcceac6015479943c915e55853937dc1b
Sheila Troxel <117420486+SheTroxel@users.noreply.github.com>
2023-04-17 21:21:11 -0400

    Add files via upload

---
+++ table_confirmation.sql
@@ -0,0 +1,10 @@
+-- confirm table has been set up -----------------------------------------------------------
+SELECT *
+FROM ca_wildfire
+;
+-- Double precision data types are to accommodate the float 64 data types in the csv file --
+-- Confirm CSV files have been added correctly
+
+Select *
+From ca_wildfire
+;
