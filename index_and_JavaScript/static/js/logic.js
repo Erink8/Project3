9fe830ce6951943a380d475dd2f8387bd646a22d
Erin Clark <ehclark2020@gmail.com>
2023-04-19 13:14:44 -0500

    mapping work

--- index_and_JavaScript/static/js/logic.js
+++ index_and_JavaScript/static/js/logic.js
@@ -1,11 +1,11 @@
-//create base level map
+// Create a map object.
 var myMap = L.map("map", {
-  center: [37.33, -119.85],
-  zoom: 6,
+  center: [37.33, -119.85], //center on San Jose
+  zoom: 6
 });
-
+// Add a tile layer.
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
 // Fetch the JSON data and console log it
@@ -24,5 +24,11 @@ d3.json("https://california-wildfires-api-r6o6.onrender.com/api", function (erro
           longitude: d["Longitude"],
           month_year: d["Date: Month, Year"],
           start_date: d["Start Date"]
+        });
       });
-    })} );
+// Looping through the fireData 
+for (var i = 0; i < fireData.length; i++) {
+  var fire = fire[i];
+  var marker = L.marker([d.latitude, d.longitude]).addTo(myMap);
+  marker.bindPopup(`<h1>${fire.incident_name}</h1> <hr> <h3>Population ${fire.acres_burned.toLocaleString()}</h3>`)
+}});
