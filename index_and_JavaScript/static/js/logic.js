06a4c541ef271e15fce3546a5ccdac1946125df7
Erin Clark <ehclark2020@gmail.com>
2023-04-19 14:36:38 -0500

    mapping work

--- index_and_JavaScript/static/js/logic.js
+++ index_and_JavaScript/static/js/logic.js
@@ -1,34 +1,55 @@
-// Create a map object.
-var myMap = L.map("map", {
+function createMap(wildFires) {
+
+  // Create the tile layer that will be the background of our map.
+  var streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
+
+ // Create a baseMaps object to hold the streetmap layer.
+ var baseMaps = {
+  "Street Map": streetmap
+};
+
+// Create an overlayMaps object to hold the bikeStations layer.
+var overlayMaps = {
+  "Wild Fires": wildFires
+};
+
+// Create the map object with options.
+var map = L.map("map-id", {
   center: [37.33, -119.85], //center on San Jose
-  zoom: 6
+  zoom: 6,
+  layers: [streetmap, wildFires]
 });
-// Add a tile layer.
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
 
-// Fetch the JSON data and console log it
-d3.json("https://california-wildfires-api-r6o6.onrender.com/api", function (error, data) {
-    if (error) return console.error(error);
+  // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(map);
+}
+function createMarkers(response) {
 
-  var fireData = [];
-  data.forEach(function (d) {
-      fireData.push({
-          ID: d["ID"],
-          acres_burned: d["Acres Burned"],
-          days_duration: d["Days Duration"],
-          incident_name: d["Incident Name"],
-          latitude: d["Latitude"],
-          location: d["Location"],
-          longitude: d["Longitude"],
-          month_year: d["Date: Month, Year"],
-          start_date: d["Start Date"]
-        });
-      });
-// Looping through the fireData 
-for (var i = 0; i < fireData.length; i++) {
-  var fire = fire[i];
-  var marker = L.marker([d.latitude, d.longitude]).addTo(myMap);
-  marker.bindPopup(`<h1>${fire.incident_name}</h1> <hr> <h3>Population ${fire.acres_burned.toLocaleString()}</h3>`)
-}});
+  // Pull the "incident_name" property from response.Features.
+  var incidents = response.Features.incident_name;
+
+  // Initialize an array to hold fire markers.
+  var FireMarkers = [];
+
+  // Loop through the fire array.
+  for (var index = 0; index < Features.length; index++) {
+    var incident = Features[index];
+
+    // For each incident, create a marker, and bind a popup with the incident's name.
+    var fireMarker = L.marker([incident.latitude, incident.longitude])
+      .bindPopup("<h3>" + incident.incident_name + "<h3><h3>Capacity: " + incident.acres_burned + "</h3>");
+
+    // Add the marker to the bikeMarkers array.
+    fireMarkers.push(fireMarker);
+  }
+
+  // Create a layer group that's made from the bike markers array, and pass it to the createMap function.
+  createMap(L.layerGroup(fireMarkers));
+}
+
+// Perform an API call to the wild fire data to get the fire information. Call createMarkers when it completes.
+d3.json("https://california-fire-data-if1l.onrender.com/api").then(createMarkers);
