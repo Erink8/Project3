b588afc86a2d268f7d9dfe28453e3fd5a99b7362
SheTroxel <smtroxel@gmail.com>
2023-04-19 14:05:11 -0400

    add geo_keys.py

---
+++ geo_keys.py
@@ -0,0 +1,4 @@
+#key to connect api
+database_key = "postgresql://climate_change_user:xEZhRWSSGx2TOWeWRDuS65K0D0UXcARD@dpg-cgutndodh87vdorr2910-a.oregon-postgres.render.com/california_climate_change"
+#geo_key = "postgresql://climate_change_user:xEZhRWSSGx2TOWeWRDuS65K0D0UXcARD@dpg-cgutndodh87vdorr2910-a/california_climate_change"
+#print(database_key)
