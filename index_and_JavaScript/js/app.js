0a178d1f7a96299c78404b053c7b9892ddd6d737
nancygmz <gnancy775@gmail.com>
2023-04-18 20:39:29 -0400

    api working

--- index_and_JavaScript/js/app.js
+++ index_and_JavaScript/js/app.js
@@ -1,70 +1,63 @@
-// Define the URL of your Flask API
-var apiUrl = "https://california-wildfires-api-r6o6.onrender.com/api";
 
-// Use fetch() to make a GET request to your Flask API
-fetch(apiUrl)
-  .then(response => response.json()) // Convert the response to JSON
-  .then(data => {
-    console.log(data); // Access the data returned by the Flask API
-    // Perform actions with the data, such as updating the DOM or calling other functions
-  })
-  .catch(error => {
-    console.error("Error fetching data from API:", error);
+// Store a URL/ call api
+var URL = "https://california-wildfires-api-r6o6.onrender.com/api"
+
+// Fetch the JSON data and console log it
+d3.json(URL).then(
+  function(data) {
+    console.log(data);
   });
 
+// Create funtion that builds bubble chart
+function buildBubbleChart(sample)
+{
+     // Use the D3 library to get all the data
+     d3.json(URL).then((data) => {
 
-// // Store a URL/ call api
-// var URL = "https://california-wildfires-api-r6o6.onrender.com/api"
-
-// // Fetch the JSON data and console log it
-// d3.json(URL).then(
-//   function(data) {
-//     console.log(data);
-//   });
-
-
-// // Create funtion that builds bubble chart
-// function buildBubbleChart(sample)
-// {
-//      // Use the D3 library to get all the data
-//      d3.json(api).then((data) => {
-
-//         let sampleData = data.samples;
-
-//         // Filter the data 
-//         let filteredData = sampleData.filter(result => result.id == sample);
+          // Get the acres burned and days duration
+        let acresBurned = data.result;
+        console.log(acresBurned);
+        let daysDuration = resultData.days_duration;
+        console.log(acresBurned);
 
 
-//         // Get the first value from the data
-//         let resultData = filteredData[0];
 
-//         // Get the otu ids, lables, and sample values
-//         let otu_ids = resultData.otu_ids;
-//         let otu_labels = resultData.otu_labels;
-//         let sample_values = resultData.sample_values;
+        let sampleData = data.samples;
 
-//         // Set up trace a bubble chart
-//         let trace2 = {
-//             y: sample_values,
-//             x: otu_ids,
-//             text: otu_labels,
-//             mode: "markers",
-//             marker: {
-//                 size: sample_values,
-//                 color: otu_ids,
-//                 colorscale: "Earth"
-//             }
-//         };
-//         let bubbleData = [trace2];
+        // Filter the data 
+        let filteredData = sampleData.filter(result => result.id == sample);
 
-//         // Set up a layout for the Bubble Chart
-//         let layout = {
-//             title: "Bacteria Per Sample",
-//             hovermode: "closest",
-//             xaxis: {title: "OTU ID"}
-//         };
 
-//         // Plot the bar chart using Plotly
-//         Plotly.newPlot("bubble", bubbleData, layout)
-//     });
-// };
+        // Get the first value from the data
+        let resultData = filteredData[0];
+
+        // Get the otu ids, lables, and sample values
+        let otu_ids = resultData.otu_ids;
+        let otu_labels = resultData.otu_labels;
+        let sample_values = resultData.sample_values;
+
+        // Set up trace a bubble chart
+        let trace2 = {
+            y: sample_values,
+            x: otu_ids,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+            }
+        };
+        let bubbleData = [trace2];
+
+        // Set up a layout for the Bubble Chart
+        let layout = {
+            title: "Bacteria Per Sample",
+            hovermode: "closest",
+            xaxis: {title: "OTU ID"}
+        };
+
+        // Plot the bar chart using Plotly
+        Plotly.newPlot("bubble", bubbleData, layout)
+    });
+};
