89d4bf0ed54ff538cee208845555bdf2e5da2052
SheTroxel <smtroxel@gmail.com>
2023-04-19 12:13:37 -0400

    added json format to code

--- app.py
+++ app.py
@@ -1,43 +1,76 @@
 #Set up dependencies
-#################################################################################
 import os
 from flask import Flask, jsonify
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.automap import automap_base
 from flask_cors import cross_origin
-#DATABASE_URI = "postgres://california_fire_data_user:NbkbeOnFEw8JiagLONTh7bO66LOj3OKQ@dpg-cgs9g8qut4mcloj9clbg-a.oregon-postgres.render.com/california_fire_data"
+from geo_keys import database_key
 
-#############################################################################
-#KEY: DATABASE_URI
-#VALUE: c124d7c8d7e7f920b99ff2a334b634b2
-#API Unique Name:   projectKey
-#API Key: rnd_7h1CIcrEbpcxg6tmAaZKcC58YZ4L
-################################################################################
+
+
+# Create engine to postgress sql california wildfire data file
+engine = create_engine(database_key)
+
+# Reflect an existing database into a new model
 Base = automap_base()
 
-#engine = create_engine(os.gentenv('DATABASE_URI_E'))
-#engine = create_engine(os.getenv(DATABASE_URI))
-engine = create_engine("postgresql://california_fire_data_user:NbkbeOnFEw8JiagLONTh7bO66LOj3OKQ@dpg-cgs9g8qut4mcloj9clbg-a.oregon-postgres.render.com/california_fire_data")
+# Reflect tables
+Base.prepare(engine,reflect=True)
 
-Base.prepare(engine, reflect= True)
+# Save references to each table
+Ca_wildfire = Base.classes.ca_wildfire
+Ca_drought = Base.classes.ca_drought
 
-#Test = Base.classes.california_fire_data
+# Initialize app
+app = Flask(__name__)
 
-session = Session(engine)
+#################################################
+# Flask Routes
+#################################################
 
-app = Flask (__name__)
-
-@app.route('/*')
+# welcome page
+@app.route('/')
 def greeting():
     return 'Welcome to California Wildfire Data API'
 
+# CA wildfire data
 @app.route('/api')
-@cross_origin()
+@cross_origin() 
 def test():
-    cols = ['id','val']
-    data = session.query(Test).all()
-    result = [{col: getattr(d, col, _default=None) for col in cols} for d in data]
-    return jsonify(result=result)
+    # Create our session (link from python to sql database)
+    session = Session(engine)
 
-#print(session)
+    # Return JSON list of stations from the dataset
+    results = session.query(Ca_wildfire).all()
+    session.close()
+
+    cols = ['ID','incident_name', 'county', 'location', 'longitude', 'latitude', 'acres_burned', 'start_date', 'days_duration','month_year']
+    data = session.query(Ca_wildfire).all()
+    result = [{col: getattr(d, col) for col in cols} for d in data]
+    jsonData= {"type": "FeatureCollection", "Features": result}
+    return jsonify(jsonData)
+
+#if __name__ == "__main__":
+#    app.run(debug=True)
+
+# CA drought data
+@app.route('/api')
+@cross_origin() 
+def get_drought_data():
+    session = Session(engine)
+
+    # Return JSON list of stations from the dataset
+    results = session.query(Ca_drought).all()
+    session.close()
+
+    cols = ['drought_id','non_avg_pct', 'd0_avg_pct', 'd1_avg_pct', 'd2_avg_pct', 'd3_avg_pct', 'd4_avg_pct', 'total_acres_burned', 'month_year']
+    data = session.query(Ca_drought).all()
+    result = [{col: getattr(d, col) for col in cols} for d in data]
+    jsonData= {"type": "FeatureCollection", "Features": result}
+    return jsonify(jsonData)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+## needs geoJson format in order to map data
